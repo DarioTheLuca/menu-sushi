69e5c1682d7d7c8022ec3b3a7b59935ea8888f63
DarioDeLuca <dar-de-lu@hotmail.it>
2022-04-22 12:33:02 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/navbar"; //per usare il componente devo importarlo
+import Card from "./components/card";
+import React, { Component } from "react";
+import california from "./images/images/california.png";
+import dragon from "./images/images/dragon.png";
+import dynamite from "./images/images/dynamite.png";
+import logo from "./images/images/logo.png";
+import philadelphia from "./images/images/philadelphia.png";
+import rainbow from "./images/images/rainbow.png";
+import shrimp from "./images/images/shrimp.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    cards: [
+      { id: 0, nome: "California", prezzo: 1.99, immagine: california ,quantità: 0},
+      { id: 1, nome: "Dragon", prezzo: 3, immagine: dragon ,quantità: 0},
+      { id: 2, nome: "Dynamite", prezzo: 2.99, immagine: dynamite,quantità: 0 },
+      { id: 3, nome: "Philadelphia", prezzo: 4.99, immagine: philadelphia ,quantità: 0},
+      { id: 4, nome: "Rainbow", prezzo: 5.99, immagine: rainbow,quantità: 0},
+      { id: 5, nome: "Shrimp", prezzo: 1.99, immagine: shrimp,quantità: 0 }
+    ]
+  }
+
+  handleDelete= cardID =>{
+    const cards=this.state.cards.filter(card => card.id != cardID); //definisco la costante aggiornata
+    this.setState({cards}); //la assegno allo stato
+  }
+
+  handleIncrement=card=>{
+    const cards=[...this.state.cards];
+    const index=cards.indexOf(card);
+    //cards[index]={...card}; può essere omessa, non capisco perchè sta
+    cards[index].quantità++;
+    this.setState({cards});
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar />
+        <div className="container">
+          <h1>Cosa desideri ordinare?</h1>
+          <hr />
+          <div className="row">
+            {/* la funzione map è di javascript e permette di ciclare un array di oggetti */}
+            {this.state.cards.map(card => (
+              <Card 
+              key={card.id} 
+              card={card} 
+              onDelete={this.handleDelete} /* gli passo il metodo della classe padre attraverso le props*/ 
+              onIncrement={this.handleIncrement}
+              />
+            ))}
+          </div>
+
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css'; 
+// importiamo bootstrap dopo averlo scaricato dal prompt ( dentro la cartella del progetto) col comando npm install bootstrap
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
